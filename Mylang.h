6b21897862ce9252ab7f7b896e6dcb0ce9c965a1
smopuim2 <114752061+smopuim2@users.noreply.github.com>
2022-09-30 21:55:07 +0800

    Create Mylang.h

---
+++ Mylang.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#define start(); int a[1000]; int main(){
+#define end(); return 0; }
+#define say(x); std::cout<<x;
+#define read(x); std::cin>>x;
+#define var(x) a[x]
+#define addvar(x,y,z); x=y+z;
+#define ltto(x,y,z); if(x<y) goto z;
+#define to(x); x:;
